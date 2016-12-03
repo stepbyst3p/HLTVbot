7e9e3eb4cef32cbf4538d06354bf2ce5b747d8c4
stepbyst3p <spikerdn@gmail.com>
2016-12-04 01:06:04 +0500

    added simple "hello human" response on /upcoming message

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import HLTV from 'hltv'
-
+var HLTV = require('hltv');
 var TelegramBot = require('node-telegram-bot-api');
 var token = '255880512:AAHo9Na2lm5FEdRdn18ULhi5C_YViriUlmM';
 
 var bot = new TelegramBot(token, {
   polling: true
+});
+
+console.log("Hi. I'm ready.")
+
+bot.onText(/\/upcoming/, function(msg) {
+  var userId = msg.from.id;
+  bot.sendMessage(userId, "Hello, human 👍");
 });
