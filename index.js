38691399901d9081becc7ce9a3f86834dc4fe48a
stepbyst3p <spikerdn@gmail.com>
2016-12-04 17:42:49 +0500

    added cheerio removed hltv
    added simple parsing matches from hltv.org

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var HLTV = require('hltv');
 var TelegramBot = require('node-telegram-bot-api');
+var cheerio = require('cheerio');
+var request = require('request');
 var token = '255880512:AAHo9Na2lm5FEdRdn18ULhi5C_YViriUlmM';
-
 var bot = new TelegramBot(token, {
   polling: true
 });
 
-console.log("Hi. I'm ready.")
-
-bot.onText(/\/upcoming/, function(msg) {
-  var userId = msg.from.id;
-  bot.sendMessage(userId, "Hello, human 👍");
+var url = "http://www.hltv.org/matches/"
+request(url, function(err, res, body){
+  if(err){console.log(err);}
+  else{
+    $ = cheerio.load(body);
+    var cards = [];
+    $('.matchListBox').each(function(){
+      cards.push({
+          timeStatus:$('.matchTimeCell',this).text(),
+          team1:$('.matchTeam1Cell',this).text().replace(/\n|\s/g, ""),
+          team2:$('.matchTeam2Cell',this).text().replace(/\n|\s/g, ""),
+          link:'hltv.org' + $('.matchActionCell a',this).attr('href')
+          // url:$('a',this).attr('href')
+      });
+      console.log(cards)
+    });
+  }
 });
